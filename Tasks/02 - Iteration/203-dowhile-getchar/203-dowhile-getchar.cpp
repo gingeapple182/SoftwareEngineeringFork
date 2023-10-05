9526c76f48f5668578a5b1ba834ea05499d7a288
Oliver <129097817+gingeapple182@users.noreply.github.com>
2023-10-05 10:29:57 +0100

    did stuff

--- Tasks/02 - Iteration/203-dowhile-getchar/203-dowhile-getchar.cpp
+++ Tasks/02 - Iteration/203-dowhile-getchar/203-dowhile-getchar.cpp
@@ -3,19 +3,21 @@
 int main()
 {
     puts("Press a letter or number, then press return");
-    puts("Press q then return to quit");
+    puts("Press q or Q then return to quit");
 
     char userEnteredCharacter;
     char returnKey;
+    bool exitCondition;
 
     do
     {
         userEnteredCharacter = getchar();   //ASCII character
         returnKey = getchar();              //Return key
         printf("You typed %c\n", userEnteredCharacter);
-        //printf("You also pressed return (ASCII %d)\n", returnKey);    //Uncomment this
+        printf("You also pressed return (ASCII %d)\n", returnKey);    //Uncomment this
+        exitCondition = (userEnteredCharacter == 'q') || (userEnteredCharacter == 'Q');
 
-    } while (userEnteredCharacter != 'q');            //Repeat if condition is met - note the != operator
+    } while (!exitCondition);            //Repeat if condition is met - note the != operator
 
     puts("Finished");
 }
